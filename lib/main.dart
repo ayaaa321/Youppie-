03ed3564922e3732a881fba300def4573d20a9d7
Chahdhammani <chahd.hammami@ensia.edu.dz>
2025-11-03 20:25:03 +0100

    Integrated BottomNavBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
-import 'presentation/screens/home/feed_screen.dart';
+import 'presentation/screens/auth/login_screen.dart';
+import 'presentation/screens/auth/signup_screen.dart';
+import 'presentation/screens/profile/profile_screen.dart';
+import 'presentation/screens/settings/settings_screen.dart';
+import 'presentation/screens/help_and_support/help_and_support.dart';
+import 'presentation/screens/terms_of_service/terms_of_service.dart';
+import 'presentation/screens/privacy_policy/privacy_policy.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +18,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Flutter App',
+      title: 'Youppie',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.indigo),
-      home: FeedScreen(),
+      //home: const CreateAccountPage(), //  change to LoginScreen() if needed
+      //home: const LoginScreen(),
+      //
+      //home: const ProfileScreen(),
+      home: const SettingsScreen(),
+      //home: const  HelpSupportApp(),
+      // home: const TermsOfServiceApp(),
+      // home: const PrivacyPolicyApp(),
     );
   }
 }
